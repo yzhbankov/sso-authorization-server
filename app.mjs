c10a1166dd71a14760ea871e702b51d0b799d484
yzhbankov <yzhbankov@gmail.com>
2020-05-16 11:21:05 +0300

    add uncauch exceptions handler

--- app.mjs
+++ app.mjs
@@ -23,6 +23,24 @@ process.on('SIGINT', async () => {
     await exit();
 });
 
+process.on('unhandledRejection', error => {
+    console.error(error);
+
+    logger.fatal({
+        type  : 'UnhandledRejection',
+        error : error.stack
+    });
+});
+
+process.on('uncaughtException', error => {
+    console.error(error);
+
+    logger.fatal({
+        type  : 'UncaughtException',
+        error : error.stack
+    });
+});
+
 async function exit() {
     await RestAPI.stop();
 
